4337781f31d79a41e87f3b50d58b6b28ebc528bc
zbrachinara <zbrachinara@gmail.com>
2023-01-01 10:45:24 -0500

    handle unregistered users in navbar

--- src/components.ts
+++ src/components.ts
@@ -3,8 +3,65 @@ import './style.css'
 import icon from '/computer-team-icon.png'
 import profile_pic from '/profile-pic.png'// TODO: The actual profile picture of the user should be moved in instead of the placeholder picture
 
+const into_template = (code: string) => {
+    let elem = document.createElement('template');
+    elem.innerHTML = code;
+    return elem;
+}
+
+class ProfileInfo {
+    display_name: string;
+    handle: string;
+    profile_picture_link: string;
+
+    constructor(display_name: string, handle: string, profile_picture: string) {
+        this.display_name = display_name;
+        this.handle = handle;
+        this.profile_picture_link = profile_picture;
+    }
+}
+
+// TODO: Replace with network request
+let profile = new ProfileInfo("Polly Placeholder", "pollytheplayer", profile_pic); 
+
+let profile_element = "" // ecmascript foundation pls if statements as expressions
+if (profile == undefined) {
+    profile_element = '<div class="absolute right-2 top-6 bg-accent w-fit px-4 py-2 font-bold rounded-lg mx-auto sm:mx-0">Join</div>'
+} else {
+    profile_element = `
+    <div class="flex-none flex right-2 top-6 absolute md:space-x-2 cursor-default items-center md:items-start">
+        <p>2 🏆</p>
+        <div class="flex space-x-2 flex-none">
+            <div class="text-right hidden md:inline">
+                <p>${profile.display_name}</p>
+                <p class="italic text-sm">@${profile.handle}</p>
+            </div>
+            <img class="w-12 h-12 rounded-full" src="${profile.profile_picture_link}" alt="ok just pretend theres an amogus here">
+        </div>
+    </div>
+    `
+}
+
 let navbar_template = document.createElement('template')
 navbar_template.innerHTML = `
+<div class="sticky top-0 flex w-full h-24 bg-primary">
+    <div class="flex-none flex space-x-3 left-2 top-6 absolute cursor-pointer" onclick="location.href='/'">
+        <img src="${icon}" class="w-12 h-12 rounded-md" alt="opps looks like image doesn't exist">
+        <p class="my-auto collapse md:visible">CTFalcon</p>
+    </div>
+
+    <div class="m-auto flex md:space-x-8 space-x-2">
+        <p class="m-auto hover:bg-secondary p-2 rounded-md cursor-pointer" onclick="location.href='/challenges.html'">Challenges</p>
+        <p class="m-auto hover:bg-secondary p-2 rounded-md cursor-pointer" onclick="location.href='/leaderboard.html'">Leaderboard</p>
+        <p class="m-auto hover:bg-secondary p-2 rounded-md cursor-pointer" onclick="location.href='/team-info.html'">Team</p>
+    </div>
+
+    ${profile_element}
+</div>
+`
+
+let navbar_unregistered_template = document.createElement('template')
+navbar_unregistered_template.innerHTML = `
 <div class="sticky top-0 flex w-full h-24 bg-primary">
     <div class="flex-none flex space-x-3 left-2 top-6 absolute cursor-pointer" onclick="location.href='/'">
         <img src="${icon}" class="w-12 h-12 rounded-md" alt="opps looks like
@@ -18,16 +75,7 @@ navbar_template.innerHTML = `
         <p class="m-auto hover:bg-secondary p-2 rounded-md cursor-pointer" onclick="location.href='/team-info.html'">Team</p>
     </div>
 
-    <div class="flex-none flex right-2 top-6 absolute md:space-x-2 cursor-default items-center md:items-start">
-        <p>2 🏆</p>
-        <div class="flex space-x-2 flex-none">
-            <div class="text-right hidden md:inline">
-                <p>Polly Placeholder</p>
-                <p class="italic text-sm">@pollytheplayer</p>
-            </div>
-            <img class="w-12 h-12 rounded-full" src="${profile_pic}" alt="ok just pretend theres an amogus here">
-        </div>
-    </div>
+    <div class="right-2 top-6 absolute bg-accent w-fit px-4 py-2 font-bold rounded-lg mx-auto sm:mx-0">Join</div>
 </div>
 `
 
